07cdf3b81d2fab5015d654324df2b8d31201107c
Jason Paul Miller <jamiller@cs.nmsu.edu>
2024-11-22 15:32:48 -0700

    init build

---
+++ my-portfolio/src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => (
+  <section id="about">
+    <h2>About Me</h2>
+    <p>Hello! I'm Jason, a software developer passionate about creating intuitive user experiences.</p>
+  </section>
+);
+
+export default About;
---
+++ my-portfolio/src/components/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Contact = () => (
+  <section id="contact">
+    <h2>Contact Me</h2>
+    <p>Email: <a href="mailto:jason@example.com">jason@example.com</a></p>
+  </section>
+);
+
+export default Contact;
