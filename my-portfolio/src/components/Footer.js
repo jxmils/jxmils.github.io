8d38033003a1ecea99a0f81bcb9def0027894744
Jason Paul Miller <jasonversamiller@gmail.com>
2025-02-02 02:06:14 -0700

    Updates

--- my-portfolio/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="footer">
-    <p>© 2024 Jason Miller | Secret Clearance | Languages: English, Spanish</p>
-  </footer>
-);
-
-export default Footer;
